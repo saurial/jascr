5df71e59f435619b2862760a3d061e6c35171ad4
Lex2202 <example@gmail.com>
2020-12-25 21:14:45 +0300

    cicls.js_While

--- cicls.js
+++ cicls.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+
+let personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false
+};
+
+let i = 0;
+
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцение его?', '');
+          i++;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }   
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB); 
