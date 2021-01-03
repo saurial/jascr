68802243e1223f87ded3b816060108cf136aaa01
Lex2202 <example@gmail.com>
2021-01-03 13:56:06 +0300

    add priperty

---
+++ property.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let str = "test";
+
+let str1 = str.toUpperCase();
+
+console.log(str);
+console.log(str1);
+
+const fruit = "Всем привет фрукты";
+console.log(fruit.indexOf("т"));
+
+const logger = "Привет дебил";
+console.log(logger.slice(7, 9));
+
+const num = "123.345";
+console.log(parseFloat(num));
